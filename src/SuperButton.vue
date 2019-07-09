2ef1b313d3dd71734cf96964f3f043a82be387c9
Sergiu <chilatsergiu@gmail.com>
2019-07-10 00:07:03 +0300

    Init

---
+++ src/SuperButton.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'SuperButton',
+  data () {
+    return {
+      title: 'i am super Button'
+    }
+  }
+}
+</script>
+<template>
+    <div>
+        <button>{{title}}</button>
+    </div>
+</template>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SuperButton from "SuperButton"
+
+export default {
+  install(Vue, options) {
+    Vue.component("super-button", SuperButton)
+  }
+};
